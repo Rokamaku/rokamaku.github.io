52b49ac1234298c36c9729b3ec1105ee14efbee3
rokamaku <hoanganh1396@outlook.com>
2021-11-11 14:23:48 +0700

    remove redundant after rebase

--- _next/static/chunks/6728d85a-e4d5d105deb23d08671c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[158],{5155:function(t,n,e){e.d(n,{_t2:function(){return i}});var u=e(4405);function i(t){return(0,u.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"polygon",attr:{fillRule:"evenodd",points:"12 16.667 5 22 8 14 2 9.5 9.5 9.5 12 2 14.5 9.5 22 9.5 16 14 19 22"}}]})(t)}}}]);
