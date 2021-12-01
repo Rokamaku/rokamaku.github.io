8cd4ab764202c7c7b09cf085c33e350123287a61
rokamaku <hoanganh1396@outlook.com>
2021-12-01 12:51:36 +0700

    add export pdf

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import styles from '../styles/Home.module.css';
 import { Cv, CvData } from '../app/cv';
 import MyCvData from '../../public/resume.json';
+import { FaFilePdf, FaRegFilePdf } from 'react-icons/fa';
 
 const Home: NextPage = () => {
   const { basics }: CvData = MyCvData;
@@ -18,6 +19,10 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <a style={{ marginLeft: 'auto' }} target="_blank" rel="noopener" href="/CV_AnhBui_FullStackDev.pdf" >
+          {/* <img width="36px" style={{ float: 'right' }} src="icons8-export-pdf-48.png" alt="Export to Pdf" /> */}
+          <FaFilePdf  size={30} />
+          </a>
         <Cv data={MyCvData} />
       </main>
     </div>
